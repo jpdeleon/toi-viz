12d743058ee96ed013dde4ae58dc8bdc0ce0afbe
Jerome de Leon <jpdeleon.bsap@gmail.com>
2022-09-15 12:35:40 +0900

    update django admin

--- djangoapp/mysite/polls/views.py
+++ djangoapp/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 #added for media
@@ -33,7 +34,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
+        #"""Return the last five published questions excluding those set to be published in the future."""
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 #def detail(request, question_id):
 #    return HttpResponse("You're looking at question %s." % question_id)
@@ -53,6 +58,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
 #    response = "You're looking at the results of question %s."
