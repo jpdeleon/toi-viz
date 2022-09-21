b7d79625dde3e88362f0bfe21133307682b29c7f
Jerome de Leon <jpdeleon.bsap@gmail.com>
2022-09-21 16:38:11 +0900

    update

--- djangoapp/django-polls/polls/forms.py
+++
@@ -1,8 +0,0 @@
-#added for media
-from django import forms
-from .models import Image
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model= Image
-        fields= ["name", "imagefile"]
